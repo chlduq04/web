57eb24b48d58d04e2cc8a8bf6d2e3274eb2812df
chlduq04 <chlduq04@gmail.com>
2012-08-06 12:08:12 +0900

    Update outlogin.php

--- outlogin.php
+++ outlogin.php
@@ -16,14 +16,14 @@ function join(){
 	if($_SESSION['userID']=="") {
 		echo "<input name='id'> <br>";
 		echo "<input type='password' name='pwd'> <br>";
-		echo "<input type='submit' value='�α���'>";
+		echo "<input type='submit' value='로그인'>";
 		echo "</form>";
 	}
 	else {
 		$userID = $_SESSION['userID'];
 		$userName = $_SESSION['userName'];
 		?>
-	<script>alert("<?echo($userID)?> ���� �������̰� <?echo($userName)?>�� �ݰ����ϴ�");</script>
+	<script>alert("<?echo($userID)?> 님이 접속중이고 <?echo($userName)?>님 반갑습니다");</script>
 	</form>
 	<button name=logout onclick="logout()">logout</button>
 	<?
